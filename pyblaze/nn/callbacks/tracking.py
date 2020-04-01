fed70872fdbe8afc8e2f5ba3cc5e6ce31cb29abc
Oliver Borchert <borchero@icloud.com>
2020-04-01 03:03:38 +0200

    Add tensorboard logger

--- pyblaze/nn/callbacks/tracking.py
+++ pyblaze/nn/callbacks/tracking.py
@@ -1,3 +1,4 @@
+from torch.utils.tensorboard import SummaryWriter
 from .base import TrainingCallback
 
 class NeptuneTracker(TrainingCallback):
@@ -31,3 +32,43 @@ class NeptuneTracker(TrainingCallback):
     def after_epoch(self, metrics):
         for k, v in metrics.to_dict().items():
             self.experiment.log_metric(k, v)
+
+
+class TensorboardTracker(TrainingCallback):
+    """
+    The tensorboard tracker can be used to track experiments with tensorboard. The summary writer
+    is available as `writer` property on the tracker.
+    """
+
+    def __init__(self, local_dir):
+        """
+        Initializes a new Tensorboard tracker logging to the specified directory.
+
+        Parameters:
+        -----------
+        local_dir: str
+            The directory to log to.
+        """
+        self.writer = SummaryWriter(local_dir)
+        self.batch_counter = None
+        self.epoch_counter = None
+
+    def before_training(self, model, num_epochs):
+        self.batch_counter = 0
+        self.epoch_counter = 0
+
+    def after_batch(self, train_loss):
+        if isinstance(train_loss, (list, tuple)):
+            for i, val in enumerate(train_loss):
+                self.writer.add_scalar(f'batch_train_loss_{i}', val, self.batch_counter)
+        elif isinstance(train_loss, dict):
+            for k, val in train_loss.items():
+                self.writer.add_scalar(f'batch_train_loss_{k}', val, self.batch_counter)
+        else:
+            self.writer.add_scalar(f'batch_train_loss', train_loss, self.batch_counter)
+        self.batch_counter += 1
+
+    def after_epoch(self, metrics):
+        for k, v in metrics.to_dict().items():
+            self.writer.add_scalar(k, v, self.epoch_counter)
+        self.epoch_counter += 1
