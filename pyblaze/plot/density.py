1e476818858009909fa14e5cc027ae1992707d01
Oliver Borchert <borchero@icloud.com>
2020-05-29 04:08:46 +0200

    Update plotting utils

--- pyblaze/plot/density.py
+++ pyblaze/plot/density.py
@@ -1,16 +1,17 @@
 import torch
 import matplotlib.pyplot as plt
 
-def density_plot2d(distribution, x_range=(-1, 1), y_range=(-1, 1), resolution=500,
-                   cmap='Blues', **kwargs):
+def density_plot2d(callback, x_range=(-1, 1), y_range=(-1, 1), resolution=500,
+                   cmap='Blues', threshold=None, **kwargs):
     """
-    Generates a scatter plot visualizing the distribution's density in the given 2D region.
+    Generates a scatter plot visualizing a distribution's density in the given 2D region.
 
     Parameters
     ----------
-    distribution: torch.nn.Module
-        The distribution modeled as PyTorch module. Must take a `torch.Tensor [N, 2]` (number of
-        evaluation points N) as input and return the log-probabilities as `torch.Tensor [N]`.
+    callback: callable
+        The distribution for which the probability is evaluated. Potentially given as PyTorch
+        module. Must take a `torch.Tensor [N, 2]` (number of evaluation points N) as input and
+        return the output as `torch.Tensor [N]`.
     x_range: tuple of (float, float), default: (-1, 1)
         The range to visualize in x-dimension.
     y_range: tuple of (float, float), default: (-1, 1)
@@ -20,6 +21,8 @@ def density_plot2d(distribution, x_range=(-1, 1), y_range=(-1, 1), resolution=50
         is therefore given by the squared resolution.
     cmap: str, default: 'gist_heat'
         The matplotlib colorbar to use for visualization.
+    threshold: float, default: None
+        A minimum value that needs to be surpassed in order for a scatter point to be plotted.
     kwargs: keyword arguments
         Additional arguments passed to the :code:`scatter` method.
     """
@@ -30,7 +33,12 @@ def density_plot2d(distribution, x_range=(-1, 1), y_range=(-1, 1), resolution=50
 
     measure_points = torch.stack([x.reshape(-1), y.reshape(-1)], dim=1)
     with torch.no_grad():
-        log_probs = distribution(measure_points)
+        values = callback(measure_points).numpy()
 
-    vis_points = measure_points.numpy().T
-    plt.scatter(*vis_points, c=log_probs.exp().numpy(), cmap=cmap, **kwargs)
+    if threshold is not None:
+        mask = values > threshold
+    else:
+        mask = torch.ones(values.shape[0], dtype=torch.bool)
+
+    vis_points = measure_points[mask].numpy().T
+    return plt.scatter(*vis_points, c=values[mask], cmap=cmap, **kwargs)
