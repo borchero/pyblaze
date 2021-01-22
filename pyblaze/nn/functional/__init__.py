b4d9a6f31b5538d1dd8e75771ad24ece2614aec1
Oliver Borchert <oliver@taskfleet.io>
2021-01-22 20:28:20 +0100

    Remove superfluous import

--- pyblaze/nn/functional/__init__.py
+++ pyblaze/nn/functional/__init__.py
@@ -1,4 +1,4 @@
-from .densities import log_prob_standard_normal, log_prob_standard_gmm, generate_random_gmm
+from .densities import log_prob_standard_normal, log_prob_standard_gmm
 from .softgrad import gumbel_softmax, softround
 from .metrics import accuracy, precision, average_precision, recall, roc_auc_score, pr_auc_score, \
     f1_score
